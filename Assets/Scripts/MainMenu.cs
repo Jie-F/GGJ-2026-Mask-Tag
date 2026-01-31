6040b04b00fb875634caab7c60993e70aeba6494
Jie-F <78284005+Jie-F@users.noreply.github.com>
2026-01-31 14:39:19 -0700

    Hook up basic settings

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -3,10 +3,25 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    [Header("Menus")]
+    public GameObject mainMenu;
+    public GameObject settingsMenu;
+
     void Start()
     {
-        
+        ShowMainMenu();
+    }
+
+    public void ShowMainMenu()
+    {
+        mainMenu.SetActive(true);
+        settingsMenu.SetActive(false);
+    }
+
+    public void ShowSettingsMenu()
+    {
+        mainMenu.SetActive(false);
+        settingsMenu.SetActive(true);
     }
 
     // Update is called once per frame
---
+++ Assets/Scripts/SettingsMenu.cs
@@ -0,0 +1,41 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SettingsMenu : MonoBehaviour
+{
+    [Header("UI References")]
+    public Slider volumeSlider;
+    public Slider sensitivitySlider;
+    public Toggle fullscreenToggle;
+
+    void Start()
+    {
+        // Load saved settings or defaults
+        volumeSlider.value = PlayerPrefs.GetFloat("MasterVolume", 1f);
+        sensitivitySlider.value = PlayerPrefs.GetFloat("MouseSensitivity", 2f);
+        fullscreenToggle.isOn = PlayerPrefs.GetInt("Fullscreen", 1) == 1;
+
+        ApplyVolume(volumeSlider.value);
+        ApplySensitivity(sensitivitySlider.value);
+        ApplyFullscreen(fullscreenToggle.isOn);
+    }
+
+    // ===== APPLY METHODS =====
+
+    public void ApplyVolume(float value)
+    {
+        AudioListener.volume = value;
+        PlayerPrefs.SetFloat("MasterVolume", value);
+    }
+
+    public void ApplySensitivity(float value)
+    {
+        PlayerPrefs.SetFloat("MouseSensitivity", value);
+    }
+
+    public void ApplyFullscreen(bool isFullscreen)
+    {
+        Screen.fullScreen = isFullscreen;
+        PlayerPrefs.SetInt("Fullscreen", isFullscreen ? 1 : 0);
+    }
+}
